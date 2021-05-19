b55a58a145a4a950570aeb31386fc3867e631bdd
jesse <jesse9e36ah@163.com>
2021-05-19 15:23:05 +0800

    Drawer add onClose

--- src/Edit/Drawer/index.js
+++ src/Edit/Drawer/index.js
@@ -1,10 +1,19 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./index.module.css";
 import Switch from "components/Switch";
 import { Context, DispatchContext, update } from "Provider";
 
 const cx = classNames.bind(styles);
+
+const onCloseEvent = (onClose) => ({
+  add: () => {
+    document.addEventListener("click", onClose, false);
+  },
+  remove: () => {
+    document.removeEventListener("click", onClose, false);
+  },
+});
 
 const Drawer = ({ wrapCls, persist = false }) => {
   const [visible, setVisible] = useState(false);
@@ -17,8 +26,34 @@ const Drawer = ({ wrapCls, persist = false }) => {
     dispatch(update({ enableDemo: !enableDemo }));
   };
 
+  const stopOnClosePropagation = (event) => {
+    event.stopPropagation();
+  };
+
+  useEffect(() => {
+    const onClose = () => {
+      console.log("onClose");
+      setVisible(false);
+    };
+
+    // noop close
+    let close = { add: () => {}, remove: () => {} };
+
+    if (visible) {
+      close = onCloseEvent(onClose);
+      close.add();
+    }
+
+    return () => {
+      close.remove();
+    };
+  }, [visible]);
+
   return (
-    <div className={cx("content", wrapCls, { visible, persist })}>
+    <div
+      className={cx("content", wrapCls, { visible, persist })}
+      onClick={stopOnClosePropagation}
+    >
       <form className={cx("form")}>
         <div className={cx("form-item")}>
           <label className={cx("label")}>preview Demo</label>
