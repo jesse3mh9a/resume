0c90191a3c098787e94e923d7fb8f4778abf48e9
jesse <jesse9e36ah@163.com>
2021-05-22 22:05:51 +0800

    update

--- src/components/FrontScale/index.js
+++ src/components/FrontScale/index.js
@@ -8,23 +8,23 @@ import styles from "./index.module.css";
 
 const cx = classNames.bind(styles);
 
+const getWidth = (ref, dispatch) => {
+  if (ref.current && ref.current.parentNode) {
+    const width = ref.current.offsetWidth;
+    const parentWidth = ref.current.parentNode.offsetWidth;
+
+    dispatch(setFrontScale(parentWidth / width));
+  }
+};
+
 const FrontScale = () => {
   const dispatch = useContext(Dispatch);
 
   const ref = useRef(null);
 
-  const getWidth = () => {
-    if (ref.current && ref.current.parentNode) {
-      const width = ref.current.offsetWidth;
-      const parentWidth = ref.current.parentNode.offsetWidth;
-
-      dispatch(setFrontScale(parentWidth / width));
-    }
-  };
-
   useEffect(() => {
-    const reportWindowSize = (event) => {
-      getWidth();
+    const reportWindowSize = () => {
+      getWidth(ref, dispatch);
     };
 
     window.addEventListener("resize", reportWindowSize);
@@ -32,11 +32,11 @@ const FrontScale = () => {
     return () => {
       window.removeEventListener("resize", reportWindowSize);
     };
-  }, []);
+  }, [dispatch]);
 
   useLayoutEffect(() => {
-    getWidth();
-  }, []);
+    getWidth(ref, dispatch);
+  }, [dispatch]);
 
   return <div ref={ref} className={cx("content")} />;
 };
